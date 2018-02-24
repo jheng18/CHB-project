75bbc8d6014b80ab95f30933f7978560bb56f0a7
jheng18 <31415506+jheng18@users.noreply.github.com>
2018-02-24 14:50:43 -0600

    Add files via upload

---
+++ csproject/visual_crime/templates/visual_crime/route1.js
@@ -0,0 +1,85 @@
+eqfeed_callback({
+    "features":[{
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[41.8024867,-87.58532489999999]},
+        "id":"001"},
+        {"type":"theft",
+            "properties":{
+                "year":2015,
+            "date":"0718",
+            "time":0960},
+        "geometry":{
+            "type":"Point",
+            "location":[ 41.8024679, -87.58778459999999]},
+        "id":"001"},
+        {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[  41.7933449, -87.5878748]},
+        "id":"001"}
+       ,
+    {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[ 41.7932886,  -87.5932648]},
+        "id":"001"}
+    ,
+ {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[  41.7931711,  -87.5932625]},
+        "id":"001"},
+         {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[ 41.7919805, -87.59405369999999]},
+        "id":"001"},
+                 {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[41.7896318,  -87.5940146]},
+        "id":"001"},
+        {
+        "type":"murder",
+        "properties":{
+            "year":2017,
+            "date":"0318",
+            "time":0900},
+        "geometry":{
+            "type":"Point",
+            "location":[41.78960720000001, -87.5964144
+]},
+        "id":"001"}
+       ]});
+
