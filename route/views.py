ea30a16efbe2432aa2cbd3eedacb7538490a4d33
jheng18 <gretasweet118@gmail.com>
2018-03-06 17:12:21 -0600

    save_my_work

--- route/views.py
+++ route/views.py
@@ -8,10 +8,7 @@ import datetime, json
 from django.urls import reverse
 from django import forms
 from django.contrib import messages
-
 from route.checkplace import *
-
-
 from route.Compute_crime_weights import *
 # from route.main import *
 from route.Subset_date_by_time import *
@@ -26,16 +23,20 @@ def index(request):
 def map(request):
     origin = request.GET['origin']
     origin = check(origin)[0]
-    org_geo = check(origin)[1]
     # if not origin:
     #     messages.error(request, 'error')
     #     return render(request, 'route/index.html')
     destin = request.GET['destination']
     destin = check(destin)[0]
-    des_geo = check(destin)[1]
-    if not destin or not origin or (origin == destin):
+    if not destin or not origin:
         messages.error(request, 'Please enter a valid address.')
         return HttpResponseRedirect( reverse('type') )
+    if destin==origin:
+        messages.error(request, 'Please enter different addresses for origin place and destination.')
+        return HttpResponseRedirect( reverse('type') )
+    else:
+        org_geo = check(origin)[1]
+        des_geo = check(destin)[1]
     mode_option = request.GET['mode']
     d = request.GET['user_date'].replace("-", ",")
     today = datetime.datetime.now().strftime('%Y,%m,%d')
@@ -51,12 +52,41 @@ def map(request):
             t = right_moment
     t = t[0].strip('0') + t[1:]
     mode_option = mode_option.swapcase()
-    pts = []
+    # pts = []
     route_option = 1
-    # pts = [41.7913123, -87.6061636, 41.7913123, -87.6051636, 41.7913123, -87.60416359999999, 41.7913123, -87.60316359999999, 41.791358, -87.60249639999999, 41.7909978, -87.6024699, 41.7899978, -87.6024699, 41.7894855, -87.6015256, 41.7892259, -87.60152049999999, 41.7892299, -87.60072249999999, 41.788229900000005, -87.60072249999999, 41.78774200000001, -87.6005843, 41.78774200000001, -87.59958429999999, 41.7877494, -87.59957589999999]
-    # return HttpResponse(des_geo)
+    pts = org_geo
+    pts += [41.7913123,
+            -87.6061636,
+            41.7913123,
+            -87.6051636,
+            41.7913123,
+            -87.60416359999999,
+            41.7913123,
+            -87.60316359999999,
+            41.791358,
+            -87.60249639999999,
+            41.7909978,
+            -87.6024699,
+            41.7899978,
+            -87.6024699,
+            41.7894855,
+            -87.6015256,
+            41.7892259,
+            -87.60152049999999,
+            41.7892299,
+            -87.60072249999999,
+            41.788229900000005,
+            -87.60072249999999,
+            41.78774200000001,
+            -87.6005843,
+            41.78774200000001,
+            -87.59958429999999,
+            41.7877494,
+            -87.59957589999999]
+    pts += des_geo
+    # return HttpResponse(pts)
     # return render(request, 'route/map.html', {'org':origin, 'des':destin, 'mode_option': mode_option})
-    return render(request, 'route/map.html', {'org':origin, 'des':destin, 'mode_option': mode_option, 'pts':pts,
-                                            'route_option': route_option})
+    # return render(request, 'route/map.html', {'org':origin, 'des':destin, 'mode_option': mode_option, 'pts':pts,
+    #                                         'route_option': route_option})
     # return render(request, 'route/map.html', {'pts':pts, 'mode_option': mode_option})
-    # return render(request, 'route/map.html', {'pts':pts,'mode_option': mode_option})
+    return render(request, 'route/map.html', {'pts':pts})
