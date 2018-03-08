f89d2b1f871579be74047c93a1f55123f1420df5
jheng18 <gretasweet118@gmail.com>
2018-03-08 11:23:02 -0600

    save_my_work

--- route/models.py
+++ route/models.py
@@ -1,7 +1,1 @@
 from django.db import models
-# from django.db import models
-# from geoposition.fields import GeopositionField
-#
-# class PointOfInterest(models.Model):
-#     name = models.CharField(max_length=100)
-#     position = GeopositionField()
